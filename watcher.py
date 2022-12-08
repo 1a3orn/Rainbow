0bfb3b342fe0cd230b204e2e3a3e94fe6fecbe4b
1a3orn <1a3orn@protonmail.com>
2022-12-07 19:32:19 -0500

    adding extra logging

--- watcher.py
+++ watcher.py
@@ -11,6 +11,8 @@ s3 = boto3.client(
 bucket_name = os.environ["AWS_S3_BUCKET"]
 folder_path = os.environ["AWS_RECORDING_DIR"]
 
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
 
 # The prefix to use for the keys of the uploaded files
 key_prefix = 'folder/'
@@ -25,14 +27,12 @@ while True:
 
         # Check if the file is a regular file (not a directory or symlink, etc.)
         if os.path.isfile(file_path):
-            # Check if the file has already been uploaded
-            if not s3.head_object(Bucket=bucket_name, Key=file):
-                # Upload the file to S3
-                with open(file_path, 'rb') as data:
-                    s3.upload_fileobj(data, bucket_name, file)
+            # Upload the file to S3
+            with open(file_path, 'rb') as data:
+                s3.upload_fileobj(data, bucket_name, file)
 
-                # Delete the file from the local filesystem
-                os.remove(file_path)
+            # Delete the file from the local filesystem
+            os.remove(file_path)
 
     # Sleep for 1 second before checking for new files again
     sleep(1)
